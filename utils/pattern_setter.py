611d6a210b73e712c6ec2a0e5e017ce977ea43e1
918273917234982734 <90898245+918273917234982734@users.noreply.github.com>
2022-11-22 10:27:06 +0900

    Update pattern_setter.py

--- utils/pattern_setter.py
+++ utils/pattern_setter.py
@@ -3,6 +3,7 @@ import torch
 import numpy as np
 import math
 import copy
+import random
 
 def pattern_setter_gradual_pat(model, mask_list, candidate, method, num_sets=8):
     patterns = [[0,0,0,0,0,0,0,0,0,  0]]
@@ -98,7 +99,7 @@ def get_pattern_gradual(patterns, arr):               # input : (?, 1, 9) / outp
     
         if(found_flag == 0):
             y = np.c_[arr[j], [1]]
-            if sum(y.tolist()[0]) != 0 : 
+            if sum(y.tolist()[0]) != 0 :
                 patterns.append(y.tolist()[0])
 
     return patterns
@@ -121,6 +122,7 @@ def get_pattern(patterns, arr):               # input : (?, 1, 9) / output : (?,
     return patterns    
 
 
+
 def top_4(arr, candidate):                     # input : (d, ch, 1, 9) / output : (d*ch, 1, 9)
     arr = arr.reshape(-1,1,9)
     arr = abs(arr)
@@ -130,7 +132,19 @@ def top_4(arr, candidate):                     # input : (d, ch, 1, 9) / output 
         x.sort()
         arr[i]=np.where(arr[i]<x[0][candidate+1], 0, 1) 
         arr[i][0][4] = 1 
-    return arr                     
+    return arr      
+
+def top_4_v(arr, candidate):                     # input : (d, ch, 1, 9) / output : (d*ch, 1, 9)
+    arr = arr.reshape(-1,1,9)
+    arr = abs(arr)
+    for i in range(len(arr)):
+        # arr[i][0][4] = 0 
+        x = arr[i].copy()
+        x.sort()
+        arr[i]=np.where(arr[i]<x[0][candidate], 0, 1) 
+        # arr[i][0][4] = 1 
+    return arr       
+      
 
 
 def pattern_setter(model, candidate, num_sets=8):
@@ -139,8 +153,26 @@ def pattern_setter(model, candidate, num_sets=8):
     for name, param in model.named_parameters():
         if name.split('.')[-1] == "weight" and len(param.shape) == 4 and 'downsample' not in name and param[0,:,:,:].shape == param[:,0,:,:].shape:
             print(f'name:{name}')
-            par=param.cpu().detach().numpy()
+            par=param.cpu().detach().numpy() 
             patterns=get_pattern(patterns, top_4(par, candidate))
+ 
+    patterns = np.array(patterns, dtype='int')
+    patterns = patterns[patterns[:,9].argsort(kind='mergesort')]
+    patterns = np.flipud(patterns)
+
+    pattern_set = patterns[:num_sets,:9]
+    # print(pattern_set)
+    
+    return pattern_set
+
+def pattern_setter_v(model, candidate, num_sets=8):
+    patterns = [[0,0,0,0,0,0,0,0,0,  0]]
+    
+    for name, param in model.named_parameters():
+        if name.split('.')[-1] == "weight" and len(param.shape) == 4 and 'downsample' not in name and param[0,:,:,:].shape == param[:,0,:,:].shape:
+            print(f'name:{name}')
+            par=param.cpu().detach().numpy() 
+            patterns=get_pattern(patterns, top_4_v(par, candidate))
  
     patterns = np.array(patterns, dtype='int')
     patterns = patterns[patterns[:,9].argsort(kind='mergesort')]
@@ -168,7 +200,7 @@ def top_4_pat(arr, pattern_set, withoc):    # input arr : (d, ch, 3, 3) or (d, c
 
             new_arr = new_arr.reshape(arr.shape)
 
-        else :
+        elif withoc == 1 : # KxKxICtxOCt
             cpy_arr = arr.copy()
             new_arr = np.zeros(cpy_arr.shape)
             tmp_pattern = []
@@ -181,14 +213,42 @@ def top_4_pat(arr, pattern_set, withoc):    # input arr : (d, ch, 3, 3) or (d, c
         
             pat_set = np.array(tmp_pattern)
 
-            for i in range(len(cpy_arr[0])):
+            for i in range(len(cpy_arr[1])):
                 pat_set = pat_set.reshape(len(pat_set), -1)
                 pat_arr = cpy_arr[:,i].reshape(-1) * pat_set
                 pat_arr = np.linalg.norm(pat_arr, axis=1)
                 pat_idx = np.argmax(pat_arr)
-                new_arr[:,i] = (cpy_arr[:,i].reshape(-1) * pat_set[pat_idx]).reshape(cpy_arr[0].shape)
+                new_arr[:,i] = (cpy_arr[:,i].reshape(-1) * pat_set[pat_idx]).reshape(cpy_arr[1].shape)
 
+                '''
+                cpy_arr = arr[0].reshape(-1, 9) 
+                new_arr = np.zeros(cpy_arr.shape)
+                pat_set = np.array(pattern_set).reshape(-1, 9)
+                for i in range(len(cpy_arr)):
+                    pat_arr = cpy_arr[i] * pat_set
+                    pat_arr = np.linalg.norm(pat_arr, axis=1)
+                    pat_idx = np.argmax(pat_arr)
+                    new_arr[i] = cpy_arr[i] * pat_set[pat_idx]
+                '''
 
+        else : # KxKxICxOC structured
+            cpy_arr = arr.copy()
+            new_arr = np.zeros(cpy_arr.shape)
+            tmp_pattern = []
+    
+            for patt in pattern_set :
+                patt = np.array(patt).reshape(3,3)
+                patt = np.tile(patt, reps = [arr.shape[0],arr.shape[1],1,1]) 
+                patt = patt.reshape(-1,9)
+                tmp_pattern.append(patt)
+        
+            pat_set = np.array(tmp_pattern)
+
+            pat_set = pat_set.reshape(len(pat_set), -1)
+            pat_arr = cpy_arr.reshape(-1) * pat_set
+            pat_arr = np.linalg.norm(pat_arr, axis=1)
+            pat_idx = np.argmax(pat_arr)
+            new_arr = (cpy_arr.reshape(-1) * pat_set[pat_idx]).reshape(arr.shape)
         return new_arr
     
     else:
@@ -245,6 +305,42 @@ def top_4_pat_swp(arr, pattern_set):   # input arr : (d, ch, 3, 3) or (d, ch, 1,
 
 
 
+""" my mistake1... should use tensor / torch calculation! (for speed)
+
+def top_4_pat(arr, pattern_set):    # input arr : (d, ch, 3, 3)   pattern_set : (6~8, 9) (9 is 3x3)
+    cpy_arr = arr.copy().reshape(-1, 1, 9)
+    new_arr = np.zeros(cpy_arr.shape)
+
+    for i in range(len(cpy_arr)):
+        max = -1
+        for j in range(len(pattern_set)):
+            pat_arr = cpy_arr[i] * pattern_set[j]
+            pat_l2 = np.linalg.norm(cpy_arr[i])
+            
+            if pat_l2 > max:
+                max = pat_l2
+                new_arr[i] = pat_arr
+        
+    new_arr = new_arr.reshape(arr.shape)
+    return new_arr
+
+
+def top_k_kernel(arr, perc):    # input (d, ch, 3, 3)
+    k = math.ceil(arr.shape[0] * arr.shape[1] / perc)
+    new_arr = arr.copy().reshape(-1, 1, 9)    # (d*ch, 1, 9)
+    l2_arr = np.zeros(len(new_arr))
+
+    for i in range(len(new_arr)):
+        l2_arr[i] = np.linalg.norm(new_arr[i]) 
+        
+    threshold = l2_arr[np.argsort(-l2_arr)[k-1]]    # top k-th l2-norm
+
+    for i in range(len(new_arr)):
+        new_arr[i] = new_arr[i] * (l2_arr[i] >= threshold)
+    
+    new_arr = new_arr.reshape(arr.shape)
+    return new_arr
+"""
 
 
 
